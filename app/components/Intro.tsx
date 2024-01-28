d8305db4b4b5c1da686a47d8b49ccc5d793bb45f
Gowtham <gajavijay8@gmai.com>
2024-01-29 00:15:40 +0530

    setup of portfolio

---
+++ app/components/Intro.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+export default function Intro() {
+  return (
+  <>
+  {/*  First Page */}
+  <div className="h-lvh md:px-24 flex items-center justify-center ">
+        <div className="w-3/5">
+          <div className="text-4xl md:text-5xl mb-10 leading-6">
+            As an aspiring developer, I'm on a journey to transform lines of
+            code into meaningful and impactful creations.
+          </div>
+          <a href="#tools">
+            {" "}
+            <span className="bg-slate-100 font-semibold px-5 py-2 rounded-md text-2xl text-gray-900">
+              {" "}
+              Know More
+            </span>
+          </a>
+        </div>
+        <div className=" hidden md:block m-auto">
+          {" "}
+          <Image src="./github-mark.svg" alt="Image" width="600" height="760" />{" "}
+        </div>
+      </div>
+  </>
+  )
+}
