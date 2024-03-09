e26cde416fc94df6ef7d730cc3c9ea7f6db15829
Gowtham <gajavijay8@gmai.com>
2024-03-09 10:28:25 +0530

    error fixed

--- app/components/Repose.tsx
+++ app/components/Repose.tsx
@@ -1,4 +1,4 @@
-export default async function Repo({ lang }) {
+export default async function Repo({ lang }:any) {
     const la: { [key: string]: number } = await fetch(lang).then(res => res.json())
     const keyss = await Object.keys(la)
     return (
